5623d0ead53dd500b2b96294224be53976d97469
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-05-29 19:03:36 +0100

    change theme

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,10 +5,12 @@ import Navbar from "./Navbar";
 export default function Layout({ children }: { children: ReactNode }) {
 
   return (
-    <div className="flex flex-col min-h-screen bg-black  text-black  transition-colors duration-300">
+    <div className="flex flex-col min-h-screen bg-white  text-black  transition-colors duration-300">
       <Navbar  />
       <main className="flex-1">{children}</main>
+      <div className="bg-black text-white">
       <Footer />
+      </div>
     </div>
   );
 }

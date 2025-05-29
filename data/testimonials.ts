cf0bf428b508932838ffe6feb3cc76f943e07bc9
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-05-29 19:04:16 +0100

    testimony

---
+++ data/testimonials.ts
@@ -0,0 +1,21 @@
+// data/testimonials.ts
+export const testimonials = [
+  {
+    id: 1,
+    name: "Aisha B.",
+    quote:
+      "Essence by Titi has completely changed my perfume game. The scent lasts all day and I always get compliments!",
+  },
+  {
+    id: 2,
+    name: "Ngozi A.",
+    quote:
+      "I was skeptical at first, but the quality is unmatched. Fast delivery and amazing packaging!",
+  },
+  {
+    id: 3,
+    name: "Tolu M.",
+    quote:
+      "Each bottle feels like luxury. I’ve tried 3 different perfumes and loved all of them.",
+  },
+];
